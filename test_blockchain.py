1569ea38f1c63117a3e38fe2b5462d960dcc6ac1
hanzemeng <hanzemeng2001518@gmail.com>
2023-05-17 08:55:05 -0700

    added user input to create a post

--- test_blockchain.py
+++ test_blockchain.py
@@ -1,13 +1,15 @@
 from control_character import *
 from blockchain import *
+import sys
+
 
 if __name__ == '__main__':
-    blockchain = Blockchain()
+	blockchain = Blockchain()
 
-    b1 = blockchain.add_block("POST", "hanzm", "EA", "End All Games")
-    blockchain.commit_block(Block.block_to_string(b1))
+	b1 = blockchain.add_block("POST", "hanzm", "EA", "End All Games")
+	blockchain.commit_block(Block.block_to_string(b1))
 
-    b2 = blockchain.add_block("POST", "hanzm", "Capcom", "Crapcom")
-    blockchain.commit_block(Block.block_to_string(b2))
+	b2 = blockchain.add_block("POST", "hanzm", "Capcom", "Crapcom")
+	blockchain.commit_block(Block.block_to_string(b2))
 
-    blockchain.print()
+	blockchain.print()
